b5ceaf55d027e6a5fb1e4bfc466882659a5c3503
Aaron Parecki <aaron@parecki.com>
2012-02-20 13:17:13 -0800

    Remove api keys

--- TrackerSample/TrackerSample/LQConfig.h
+++ TrackerSample/TrackerSample/LQConfig.h
@@ -3,8 +3,6 @@
 // enter your API Key and Secret here.
 // https://developers.geoloqi.com/
 
-//static NSString *const LQ_APIKey = @"04110bdf3dcb9413bee6471f9e09c5a4";
-//static NSString *const LQ_APISecret = @"5288fe7a4f6f024ef64bc0a48efec5fd";
 static NSString *const LQ_APIKey = @"";
 static NSString *const LQ_APISecret = @"";
 
