4a6db9e59f7933e7faee06e537f97da658b64f42
Aaron Parecki <aaron@parecki.com>
2012-02-20 12:57:44 -0800

    Updated SDK with correct git build version

--- GeoloqiSDK/LQSDKVersion.h
+++ GeoloqiSDK/LQSDKVersion.h
@@ -8,5 +8,6 @@
 
 static NSString *const LQSDKVersionString = @"12.051";
 
-//TODO: this build string should be automatically updated when the sdk is built!
-static NSString *const LQSDKVersionGitVersion = @"00000";
+// This build string is automatically updated when the sdk is built!
+// Make sure you have committed the code in git first
+static NSString *const LQSDKVersionGitVersion = @"fedb1fbdd2811f4846cb6c5acd34afe5784c9a70";
